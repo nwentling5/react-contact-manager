acde438b96bd8539581a040f0061f34524f4ed9e
Nic <nwentling5@gmail.com>
2017-04-05 15:27:11 -0500

    Completed lecture

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Header from './Components/Header';
+import Contacts from './Components/Contacts';
+import AddContact from './Components/AddContact';
+import {Grid, Row, Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<Grid>
+					<Row>
+						<Col xs={12} md={12} lg={12}>
+							<AddContact />
+							<Contacts />
+						</Col>
+					</Row>
+				</Grid>
+			</div>
+		);
+	}
+}
+
+export default App;
